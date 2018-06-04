fe0300c05973a268ec84f09d482255f52f671828
Estefany Salas <estefanycss@gmail.com>
2018-06-04 13:00:56 -0500

    functional and class components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBdpVvV9SAq20RswdZmDXO36R4AAAQInGs';
 
 const App = () => {
-  return <div > Hi there < /div>;
+  return (
+    <div >
+      <SearchBar />
+    < /div>
+  );
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
