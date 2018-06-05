89a53f5dffc1955b2ec721c0b49fb9280e21e6f2
Estefany Salas <estefanycss@gmail.com>
2018-06-04 22:07:14 -0500

    video list and video detail working

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,13 +8,16 @@ class SearchBar extends Component {
     this.state = {term : ''};
   }
 
+// A controlled component has it's value set by state
+
   render () {
-    return <input onChange= {this.onInputChange} />;
+    return (
+      <div className="search-bar">
+        <input onChange= {event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 
-  onInputChange (event) {
-    console.log(event.target.value);
-  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyBdpVvV9SAq20RswdZmDXO36R4AAAQInGs';
 
-const App = () => {
-  return (
-    <div >
-      <SearchBar />
-    < /div>
-  );
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo : null
+    };
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({
+        videos: data,
+        selectedVideo: data[0]
+       });
+      // When both variables had the same name we can use this.setState({videos})
+    });
+  }
+
+  render(){
+    return (
+      <div >
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos = {this.state.videos}/>
+      < /div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
